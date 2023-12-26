fcfabff989996aa98f3ca500da0edf197efd049a
go-dev <awwalcodestar@gmail.com>
2023-12-26 01:20:10 +0100

    Feat: added a dynamic middleware for authentication check

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,21 +20,24 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	// Create a dynamic middleware chain to for session manager to wrap the routes except /static/*filepath
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	// Create a dynamic middleware chain for session manager to wrap the routes except /static/*filepath
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
+
+	// middleware for some protected routes such as Get /snippet/create, Post /snippet/create, and Post /user/logout
+	protected := dynamic.Append(app.requireAuthentication)
 
 	//Register the other application routes
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 
 	//Add five routes for authentication of user
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	//using justinas/alice to chain the middlewares
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
